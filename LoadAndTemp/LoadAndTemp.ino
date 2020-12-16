bdb322ad3f9900d3dfe82c26eddfb85f29c7e72e
elizabec <elizabec@metropolia.fi>
2020-12-16 11:32:43 +0200

    Added MQTT and WiFi connection

--- LoadAndTemp/LoadAndTemp.ino
+++ LoadAndTemp/LoadAndTemp.ino
@@ -2,53 +2,50 @@
 #include <OneWire.h>
 #include <Notes.h>
 #include <WiFiNINA.h> //library to enable use of WiFi
+#include <SPI.h>
+#include <PubSubClient.h>
+#include <arduino_secrets.h>
 
 #define addFX29 0x28
 #define MAXLEN 10
 #define TEMP_ERROR 999.999
 OneWire tempSensor(2); //Sensor pin = 2
+WiFiClient wifi;
+PubSubClient client(wifi);
 
+char mqtt_broker[] = "broker.emqx.io";
+char mqtt_client[] = "mqttjs_13cc164d";
+const int mqtt_port = 1883;
 float values[10]   = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 float temp = 0;
 float setTemp = 60;
 byte pos = 0;
 int ledGreen = 3;
 int ledYellow = 4;
+int ledRed = 5;
 int buzzer = 8;
-int choice = 0;
+int tempo = 100;
+int status = WL_IDLE_STATUS;
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
 
-// Defining the song to play should probably be done with an array of pointers, 
-// to make it possible to make the choice from the web page
-//#define VICTORY
-//#define SONGOFTIME
-//#define MARIO
 
-#if defined(VICTORY) 
-int tempo = 115;
-int tune[] = {
-  C5, 12, C5, 12, C5, 12, C5, 4, GS4, 4, AS4, 4, C5, 12, REST, 12, AS4, 12, C5, -2
+int victory[] = {C5, 12, C5, 12, C5, 12, C5, 4, GS4, 4, AS4, 4, C5, 12, REST, 12, AS4, 12, C5, -2};
 
-};
-#elif defined(SONGOFTIME)
-int tempo = 108;
-int tune[] = {
-
-  A4, 4, D4, 2, F4, 4, A4, 4, D4, 2,
+int songoftime[] = {A4, 4, D4, 2, F4, 4, A4, 4, D4, 2,
   F4, 4, A4, 8, C4, 8, NOTE_B4, 4, G4, 4, F4, 8, G4, 8, A4, 4,
   D4, 4, C4, 8, E4, 8, D4, -1
 };
-#elif defined(MARIO)
-int tempo = 200;
-int tune[] = {E5,8, E5,8, REST,8, E5,8, REST,8, C5,8, E5,4, //1
+
+int mario[] = {E5,8, E5,8, REST,8, E5,8, REST,8, C5,8, E5,4, //1
   G5,4, REST,4, G4,8, REST,4, 
   C5,-4, G4,8, REST,4, E4,-4, // 3
   A4,4, NOTE_B4,4, AS4,8, A4,4,
   G4,-8, E5,-8, G5,-8, A5,4, F5,8, G5,8,
   REST,8, E5,4,C5,8, D5,8, NOTE_B4,-4
 };
-#else //This was very important to implement, trust me
-int tempo = 115;
-int tune[] = {D5, 2, E5, 8, FS5, 8, D5, 8, E5, 8, E5, 8, E5, 8, FS5, 8, E5, 4, A4, 4,
+
+int deftune[] = {D5, 2, E5, 8, FS5, 8, D5, 8, E5, 8, E5, 8, E5, 8, FS5, 8, E5, 4, A4, 4,
   REST, 2, NOTE_B4, 8, CS5, 8, D5, 8, NOTE_B4, 8, REST, 8, E5, 8, FS5, 8, E5, -4, 
   
   A4, 16, NOTE_B4, 16, D5, 16, NOTE_B4, 16, FS5, -8, FS5, -8, E5, -4, 
@@ -59,7 +56,6 @@ int tune[] = {D5, 2, E5, 8, FS5, 8, D5, 8, E5, 8, E5, 8, E5, 8, FS5, 8, E5, 4, A
   A5, 4, CS5, 8, D5, -8, CS5, 16, NOTE_B4, 8, A4, 16, NOTE_B4, 16, D5, 16, NOTE_B4, 16,
   D5, 4, E5, 8, CS5, -8, NOTE_B4, 16, A4, 4, A4, 8, E5, 4, D5, 2
 };
-#endif
 
 float readTemp() {
   byte data[9];
@@ -86,11 +82,64 @@ float readTemp() {
   return tempC;
 }
 
+void printCurrentNet(){
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+}
+
+void connSetup() {
+  Serial.print("Connecting WiFi...");
+  while(status != WL_CONNECTED){
+    status = WiFi.begin(ssid, pass);
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected to WiFi!");
+  printCurrentNet();
+  /*client.setServer(mqtt_broker, mqtt_port);
+  Serial.println("Connecting MQTT...");
+  while(!client.connected()){
+    Serial.println(client.state());
+    delay(1000);
+  }
+  Serial.println("Connected to MQTT!");
+  client.setCallback(callback);
+  client.subscribe("kettle/start");
+  client.subscribe("kettle/heatTemp");*/
+  client.setServer(mqtt_broker, mqtt_port);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT....");
+    if (client.connect(mqtt_client)) {
+      Serial.println("MQTT Connected!");
+    }
+    else {
+      Serial.print("failed, state ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+  client.publish("kettle/connected", "test");
+  client.subscribe("kettle/start" ,{qos: 2});
+  client.subscribe("kettle/heatTemp", {qos: 2});
+}
+
 void setup() {
   Wire.begin();
+  connSetup();
   Serial.begin(9600);
   pinMode(ledGreen, OUTPUT);
   pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
 }
 
 void loop() {
@@ -137,7 +186,7 @@ void loop() {
   temp = readTemp();
   if (temp >= setTemp) {
     digitalWrite(ledGreen, HIGH);
-    playTune();
+    playTune(1);
   }
   else {
     digitalWrite(ledGreen, LOW);
@@ -149,6 +198,7 @@ void loop() {
   else {
     digitalWrite(ledYellow, LOW);
   }
+
   Serial.print("Temperature =  ");
   Serial.print(temp, 1);
   Serial.print("  Pdisplay = ");
@@ -157,7 +207,7 @@ void loop() {
   Serial.print(Pvalue);
   Serial.print("  Average = ");
   Serial.println(pAvg());
-  playTune();
+  playTune(1);
 }
 
 float pAvg() {
@@ -168,8 +218,24 @@ float pAvg() {
   return sum / MAXLEN;
 }
 
-void playTune() {
-  
+void playTune(int choice) {
+  int * tune;
+  if(choice == 1) {
+    tempo = 115;
+    tune = victory;
+  }
+  else if(choice == 2){
+    tempo = 108;
+    tune = songoftime;
+  }
+  else if(choice == 3){
+    tempo = 200;
+    tune = mario;
+  }
+  else{
+    tempo = 115;
+    tune = deftune;
+  }
   int notes = sizeof(tune) / sizeof(tune[0]) / 2;
   int wholenote = (60000 * 4) / tempo; //Whole note in ms
   int divider = 0, noteDuration = 0;
